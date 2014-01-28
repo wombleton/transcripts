2e34bfba39c3a64fefbe5ad8148a3d3c4672281e
Rowan Crawford <wombleton@gmail.com>
2014-01-29 11:24:47 +1300

    Parse votes

--- test/unit/abortion_supervisory_committee.js
+++ test/unit/abortion_supervisory_committee.js
@@ -1,4 +1,5 @@
-var helper = require('../helper');
+var _ = require('underscore'),
+    helper = require('../helper');
 
 exports['extracts title'] = function(test) {
   helper.parse(__filename, function(err, speech) {
@@ -37,7 +38,29 @@ exports['parse debate section'] = function(test) {
     test.equals(speech.by, 'Hon MARK BURTON (Minister of Justice)');
     test.equals(speech.content, 'I move,');
 
-    vote = section.votes[1];
+    vote = section.events[1];
+    test.equals(vote.type, 'personalvote');
+    test.equals(vote.call, 'A personal vote was called for on the question');
+    test.equals(vote.question, 'That the motion be amended by omitting the words “Dr Rosemary Jane Fenwicke of Wellington”, and substituting the words “Dr Ate Moala of Wellington”.');
+    test.ok(_.isArray(vote.ayes));
+    test.equals(vote.ayes.length, 36);
+    test.equals(vote.ayes[0].name, 'Ardern');
+    test.equals(vote.ayes[0].teller, undefined);
+    test.equals(vote.ayes[0].present, true);
+
+    test.equals(vote.ayes[1].name, 'Copeland');
+    test.equals(vote.ayes[1].teller, undefined);
+    test.equals(vote.ayes[1].present, false);
+
+    test.equals(vote.ayes[35].name, 'Tolley');
+    test.equals(vote.ayes[35].present, true);
+    test.equals(vote.ayes[35].teller, true);
+
+    test.ok(_.isArray(vote.noes));
+
+    test.equals(vote.result, 'Amendment not agreed to.');
+
+    test.equals(section.events[3].abstentions.length, 5);
 
     test.done();
   });
