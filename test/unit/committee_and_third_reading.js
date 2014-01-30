db22342a6f4a41ac3e92ff3829e99b2a087125c3
Rowan Crawford <wombleton@gmail.com>
2014-01-30 14:45:29 +1300

    Extract committee/third reading html

---
+++ test/unit/committee_and_third_reading.js
@@ -0,0 +1,38 @@
+var _ = require('lodash'),
+    helper = require('../helper');
+
+exports['extracts debate'] = function(test) {
+  helper.parse(__filename, function(err, debate) {
+    var committee,
+        contribution,
+        reading,
+        speech,
+        readingSpeech;
+
+    test.equals(debate.title, 'Bail Amendment Bill — In Committee, Third Reading');
+
+    test.equals(debate.sections.length, 2);
+
+    committee = _.first(debate.sections);
+
+    test.equals(_.first(committee.headings), 'Bail Amendment Bill');
+
+    speech = _.first(committee.events);
+
+    test.equals(speech.heading, 'In Committee');
+
+    contribution = _.last(committee.events);
+
+    test.equals(_.first(contribution.items), 'Parts 1 and 2, and clauses 1 to 3');
+
+    reading = _.last(debate.sections);
+
+    test.equals(_.first(reading.headings), 'Third Reading');
+
+    readingSpeech = _.first(reading.events);
+
+    test.equals(_.first(readingSpeech.speeches).by, 'Hon JUDITH COLLINS (Minister of Justice)');
+
+    test.done();
+  });
+};
